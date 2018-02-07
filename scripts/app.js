663d7ff0538c04fad7329932b90dbda6a6312dca
Michael Lai <michael.lai@adelphi.digital>
2018-02-07 17:33:09 +1100

    Progress

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,34 @@
 angular.module('players', [])
 
-.controller('PlayersController', function($scope) {
+.controller('PlayersController', ['server', '$q'], function($scope, server, $q) {
 	
-	
+	var foo = server.get('../data/players.json');
+	console.log(foo);
+    $q.all([getObligationOptions])
+    	.then(function() {
+    		console.log(results);
+    	})
+    	.catch(function() {
 
-});
+    	});
+})
+
+.factory('server', ['$http', function($http) {
+
+    return {
+        /**
+         * Performs a get request to the specified location.
+         * 
+         * @public 
+         * @param {string} url The path to obtain data from.
+         * @returns {Promise} returns a promise.
+         */
+        get: function(url) {
+            return $http({
+                method: 'GET',
+                url: url
+            });
+        }
+    };
+
+}]);
