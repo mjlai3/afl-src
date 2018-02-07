7fa6fd625b793e27ec2df89772f98d2be2e5d34c
Michael Lai <michael.lai@adelphi.digital>
2018-02-07 21:04:18 +1100

    Progress

--- scripts/controllers/PlayersController.js
+++ scripts/controllers/PlayersController.js
@@ -1,12 +1,15 @@
 'use strict';
 
 angular.module('app.controllers').controller('PlayersController', ['$scope', 'server', '$q', function($scope, server, $q) {
+
+	$scope.players = [];
 	
 	var foo = server.get('../data/players.json');
     $q.all([foo])
     	.then(function(results) {
     		var players = results[0].data.players;
-    		console.log(players);
+    		$scope.players = players;
+    		console.log($scope.players);
     	})
     	.catch(function() {
     		console.log('Error fetching data.');
